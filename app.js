7c7b218848ea2c965c5f52b9ac98356f9c77a41a
AnishSingh15 <anish15may@gmail.com>
2022-12-26 22:25:43 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,187 @@
+/* -----------------------------------------------
+/* How to use? : Check the GitHub README
+/* ----------------------------------------------- */
+
+/* To load a config file (particles.json) you need to host this demo (MAMP/WAMP/local)... */
+/*
+particlesJS.load('particles-js', 'particles.json', function() {
+  console.log('particles.js loaded - callback');
+});
+*/
+
+/* Otherwise just put the config content (json): */
+
+particlesJS('particles-js',
+  
+  {
+    "particles": {
+      "number": {
+        "value": 60,
+        "density": {
+          "enable": true,
+          "value_area": 800
+        }
+      },
+      "color": {
+        "value": "#ffffff"
+      },
+      "shape": {
+        "type": "circle",
+        "stroke": {
+          "width": 0,
+          "color": "#000000"
+        },
+        "polygon": {
+          "nb_sides": 5
+        },
+        "image": {
+          "src": "img/github.svg",
+          "width": 100,
+          "height": 100
+        }
+      },
+      "opacity": {
+        "value": 0.5,
+        "random": false,
+        "anim": {
+          "enable": false,
+          "speed": 1,
+          "opacity_min": 0.1,
+          "sync": false
+        }
+      },
+      "size": {
+        "value": 5,
+        "random": true,
+        "anim": {
+          "enable": false,
+          "speed": 40,
+          "size_min": 0.1,
+          "sync": false
+        }
+      },
+      "line_linked": {
+        "enable": true,
+        "distance": 150,
+        "color": "#ffffff",
+        "opacity": 0.4,
+        "width": 1
+      },
+      "move": {
+        "enable": true,
+        "speed": 5,
+        "direction": "none",
+        "random": false,
+        "straight": false,
+        "out_mode": "out",
+        "attract": {
+          "enable": false,
+          "rotateX": 600,
+          "rotateY": 1200
+        }
+      }
+    },
+    "interactivity": {
+      "detect_on": "canvas",
+      "events": {
+        "onhover": {
+          "enable": true,
+          "mode": "repulse"
+        },
+        "onclick": {
+          "enable": true,
+          "mode": "push"
+        },
+        "resize": true
+      },
+      "modes": {
+        "grab": {
+          "distance": 400,
+          "line_linked": {
+            "opacity": 1
+          }
+        },
+        "bubble": {
+          "distance": 400,
+          "size": 40,
+          "duration": 2,
+          "opacity": 8,
+          "speed": 3
+        },
+        "repulse": {
+          "distance": 200
+        },
+        "push": {
+          "particles_nb": 4
+        },
+        "remove": {
+          "particles_nb": 2
+        }
+      }
+    },
+    "retina_detect": true,
+    "config_demo": {
+      "hide_card": false,
+      "background_color": "#b61924",
+      "background_image": "",
+      "background_position": "50% 50%",
+      "background_repeat": "no-repeat",
+      "background_size": "cover"
+    }
+  }
+
+);
+
+//barba
+
+// const logo = document.querySelector("#logo");
+//  const home = document.querySelector("#ho");
+//   barba.init({
+//     views: [
+//       {
+//         namespace: "home",
+//         beforeEnter(){
+//           home.href = "./index.html"
+//         }
+//       },
+//       {
+//         namespace: "teams",
+//         beforeEnter(){
+//           home.href = "../index.html"
+//         }
+//       }
+//     ],
+//     transitions: [
+//       {
+//         leave({ current, next}) {
+//           let done = this.async();
+//           ////Animation
+//           const tl = gsap.timeline({ defaults: { ease: "power2.out" }});
+
+//           tl.fromTo(current.container, 1, { opacity: 1 }, { opacity: 0 });
+//           tl.fromTo(
+//             ".swipe",
+//             0.75,
+//             { x: "-100%" },
+//             { x: "0%", onComplete: done },
+//             "-=0.2"
+//           );
+//         },
+//         enter({ current, next}) {
+//           let done = this.async();
+//           ////Animation
+//           const tl = gsap.timeline({ defaults: { ease: "power2.out" }});
+//           tl.fromTo(
+//             ".swipe",
+//             1,
+//             { x: "0%" },
+      
+//             { x: "100%", stagger: 0.23, onComplete: done }
+//           );
+//           tl.fromTo(next.container, 1, { opacity: 0 }, { opacity: 1 });
+//         }
+//       }
+//     ]
+    
+    
+//     })
